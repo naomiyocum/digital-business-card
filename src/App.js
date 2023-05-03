d2955eb8c6d570413a0100ade2d2bf91f83d9ba6
Naomi Yocum <naomiyocum24@gmail.com>
2023-05-03 13:39:36 -0600

    Add footer icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Info from './components/Info'
 import About from './components/About'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <div>
         <Info />
         <About />
+        <Footer />
       </div>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,17 @@
-// Github and Instagram and Twitter buttons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+
+
+export default function Footer() {
+  return (
+    <div className="icons">
+        <a className="icon--link" href="https://github.com/naomiyocum" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faGithub} size="lg"></FontAwesomeIcon>
+        </a>
+        <a className="icon--link" href="https://twitter.com/6nomi4" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faTwitter} size="lg"></FontAwesomeIcon>
+        </a>
+    </div>
+  )
+}
