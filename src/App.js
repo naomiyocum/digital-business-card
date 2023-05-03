3bf2fd044d372ec620f8b51fa72fae8fc1e0302e
Naomi Yocum <naomiyocum24@gmail.com>
2023-05-03 09:21:30 -0600

    Add About section with tech stack

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Info from './components/Info'
+import About from './components/About'
 
 export default function App() {
   return (
     <div className="App">
       <div>
         <Info />
+        <About />
       </div>
     </div>
   );
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+  <div className="about">
+    <h1 className="about--title">About</h1>
+    <p className="about--description">I am a backend developer with a passion for writing clean, efficient, 
+      and maintainable code. I love tackling complex problems and 
+      delivering robust solutions while following best practices.</p>
+    <p className="about--techstack">| Ruby on Rails | RDBMS (Postgres) | APIs | HTML | CSS | JavaScript | React |</p>
+  </div>
+  )
+}
