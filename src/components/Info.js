5c22770d0df86cddf4d83a20bce4bd5a3f49ab35
Naomi Yocum <naomiyocum24@gmail.com>
2023-05-03 09:21:20 -0600

    Add fontawesome icon

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,8 +1,17 @@
-// picture, name, title, email
+// picture, email
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export default function Info() {
   return (
-    <div>
-      <img src="../images/headshot.png" alt="Naomi's Headshot" />
+    <div className="info">
+      <img className="info--img" src="../images/headshot.png" alt="Naomi's Headshot"></img>
+      <img className="info--img" src="./images/headshot.png" alt="Naomi's Headshot"></img>
+      <h1 className="info--fullname">Naomi Yocum</h1>
+      <h3 className="info--title">Software Developer</h3>
+      <a className="info--email" href="mailto:naomiyocum24@gmail.com">
+        <FontAwesomeIcon icon={faEnvelope} size="lg"></FontAwesomeIcon>
+      </a>
     </div>
   )
 }
