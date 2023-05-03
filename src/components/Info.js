4506ac5a3e7622b69dd1dce9310531f62da35b8c
Naomi Yocum <naomiyocum24@gmail.com>
2023-05-03 10:10:20 -0600

    save fontawesome brands

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,6 +1,7 @@
 // picture, email
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Info() {
   return (
@@ -9,9 +10,20 @@ export default function Info() {
       <img className="info--img" src="./images/headshot.png" alt="Naomi's Headshot"></img>
       <h1 className="info--fullname">Naomi Yocum</h1>
       <h3 className="info--title">Software Developer</h3>
-      <a className="info--email" href="mailto:naomiyocum24@gmail.com">
-        <FontAwesomeIcon icon={faEnvelope} size="lg"></FontAwesomeIcon>
-      </a>
+      <div className="info-buttons">
+        <button className="info--button">
+          <a className="info--link" href="mailto:naomiyocum24@gmail.com">
+            <FontAwesomeIcon icon={faEnvelope} size="lg"></FontAwesomeIcon>&nbsp;
+            Email
+          </a>
+        </button>&nbsp;&nbsp;&nbsp;&nbsp;
+        <button className="info--button">
+          <a className="info--link" href="https://www.linkedin.com/in/naomiyocum/" target="_blank">
+            <FontAwesomeIcon icon={faLinkedin} size="lg"></FontAwesomeIcon>&nbsp;
+            Linkedin
+          </a>
+        </button>
+      </div>
     </div>
   )
 }
