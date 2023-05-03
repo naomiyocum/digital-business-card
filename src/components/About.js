30d9493f38f74d660d4b015b2bab988681b002dd
Naomi Yocum <naomiyocum24@gmail.com>
2023-05-03 13:45:16 -0600

    Polish about me section

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,10 @@ export default function About() {
   <div className="about">
     <h1 className="about--title">About</h1>
     <p className="about--description">
-      I'm a backend developer who strives to deliver high-quality, efficient, and scalable
+      I am a backend developer who strives to deliver high-quality, efficient, and scalable
       software solutions. My approach to problem-solving involes working closely with cross-functional teams
-      to understand business needs and design effective architectures. I'm passionate about developing clean,
-      maintainable, and transparent code.
+      to understand business needs and design effective architectures. I'm enthusiastic about
+      continuous growth and consistently challenge myself to learn more.
     </p>
     <p className="about--techstack"> Ruby on Rails - RDBMS (Postgres) - APIs <br />  HTML - CSS - JavaScript - React </p>
   </div>
