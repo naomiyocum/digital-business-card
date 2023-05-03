a77dd5e4c4203ea3457559edb7f0c44793ae2dbe
Naomi Yocum <naomiyocum24@gmail.com>
2023-05-03 13:39:44 -0600

    Finish up styling

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,4 +1,3 @@
-// picture, email
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
