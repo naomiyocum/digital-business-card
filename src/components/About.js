9a21bc6e472c320ad48e62ddb103f7e26bf914a8
Naomi Yocum <naomiyocum24@gmail.com>
2023-05-03 15:01:24 -0600

    Add vw for icons

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default function About() {
       to understand business needs and design effective architectures. I'm enthusiastic about
       continuous growth and consistently challenge myself to learn more.
     </p>
-    <p className="about--techstack"> Ruby on Rails - RDBMS (Postgres) - APIs <br />  HTML - CSS - JavaScript - React </p>
+    <p className="about--techstack"> Ruby on Rails - RDB (Postgres) - APIs <br />  HTML - CSS - JavaScript - React </p>
   </div>
   )
 }
